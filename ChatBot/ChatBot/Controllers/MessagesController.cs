fc165e47efe8e7c6733e24ba76f57587ed9fbd82
marcosdassienoronha <marcosdassienoronha@gmail.com>
2017-05-17 19:50:34 -0300

    Versão 0.0
    Foi acrescentado um comentário de aviso.

--- ChatBot/ChatBot/Controllers/MessagesController.cs
+++ ChatBot/ChatBot/Controllers/MessagesController.cs
@@ -5,8 +5,10 @@ using System;
 using System.Threading.Tasks;
 using System.Web.Http;
 
-
-
+//IMPORTANTE
+//Esta permitido neste arquivo apenas acrescentar código.
+//Não altere ou apague os códigos já existentes, se for fazer isto, avise e confirme com sua equipe.
+//
 namespace ChatBot.Controllers
 {
     public class MessagesController : ApiController
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Repositório para Projeto ChatBot Banco
+﻿# Repositório para Projeto ChatBot Banco
 Repositório para Projeto ChatBot Banco
+
+# IMPORTANTE
+Esta permitido neste arquivo apenas acrescentar código.
+Não altere ou apague os códigos já existentes, se for fazer isto, avise e confirme com sua equipe.
