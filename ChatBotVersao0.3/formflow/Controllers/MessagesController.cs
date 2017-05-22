322262bf2be67787be583cdd952c44cd2fc6d494
marcosdassienoronha <marcosdassienoronha@gmail.com>
2017-05-22 12:16:09 -0300

    teste

--- ChatBotVersao0.3/formflow/Controllers/MessagesController.cs
+++ ChatBotVersao0.3/formflow/Controllers/MessagesController.cs
@@ -1,18 +1,26 @@
-﻿using formflow.FormFlow;
+﻿
+using ChatBot.Serialization;
+using ChatBot.Services;
+using formflow.FormFlow;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Connector;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+ 
+
 
 namespace formflow
 {
     //[BotAuthentication]
     public class MessagesController : ApiController
     {
+
+       
         [ResponseType(typeof(void))]
 
 
@@ -21,13 +29,52 @@ namespace formflow
                                                                                                    //uma unidade de trabalho que devera ser executada, ao passar uma task em
                                                                                                    //um await, voce passa a executar esta tarefa em uma thread separada. | await é o gerenciador de thread que recebe como parametro uma task
         {
-           
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
-            {
+            string sim = string.Empty;
+            string nao = string.Empty;
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            //Método Start
+            //var resposta = await Start(activity);
+            //var msg = activity.CreateReply(resposta, "pt-BR");
+            //await connector.Conversations.SendToConversationAsync(msg);
+
+            //Método Cases
+          
+
+
+             if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+             {
                 await Conversation.SendAsync(activity, () => {
-                    return Chain.From(() => FormDialog.FromForm(Enquiry.BuildEnquiryForm));         //O metodo Chain.From faz com que construa um dialogo que vai fazer copia de outro dialogo quando iniciado, por isso precisa de uma dialogo como parametro
-                });                                                                                 //Cria uma mensagem de entrada na conversa 
-                                                                                                    //Parametros: A mensagem a ser enviada, método que fabrica o dialogo
+                     return Chain.From(() => FormDialog.FromForm(Enquiry.BuildForm));         //O metodo Chain.From faz com que construa um dialogo que vai fazer copia de outro dialogo quando iniciado, por isso precisa de uma dialogo como parametro
+                });
+
+                if (activity.Text.Equals("sim"))
+                {
+                    sim = "sim";
+                }
+                if (activity.Text.Equals("nao"))
+                {
+                    nao = "nao";
+                }
+                 
+           
+
+
+                if (!string.IsNullOrEmpty(sim))
+                {
+                    var reply = activity.CreateReply($"Obrigado! Foi um prazer negociar com você. ");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else if (!string.IsNullOrEmpty(nao))
+                {
+                    var reply = activity.CreateReply($"desconsidere entao");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else
+                {
+                    var reply = activity.CreateReply($"n entendi");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
             }
 
 
@@ -35,7 +82,9 @@ namespace formflow
             return response;
         }
 
-        
+       
+
+       
 
 
 
--- ChatBotVersao0.3/formflow/FormFlow/Enquiry.cs
+++ ChatBotVersao0.3/formflow/FormFlow/Enquiry.cs
@@ -1,18 +1,25 @@
 ﻿using Microsoft.Bot.Builder.FormFlow;
 using System;
 
+
+
+
 namespace formflow.FormFlow
 {
+    public enum YesOrNoOptions { Sim = 1, Nao };
+
     [Serializable]
     public class Enquiry
     {
-        [Prompt("Qual é o seu nome?")]
-        public string Name { get; set; }
+        
+
+        //[Prompt("Qual é o seu nome?")]
+       // public string Name { get; set; }
         [Prompt("Olá, sou um representante do Banco 1500, notamos que há algumas contas suas pendentes e gostariamos de negocia-las. A conta número 23323-90 é " +
             "pertencente a você? {||}")]
-        public bool Fala1 { get; set; }
-        [Prompt("Voce está de acordo com o combinado na negociação? {||}")]
-        public bool Fala2 { get; set; }
+        public YesOrNoOptions Fala1{ get; set; }
+       // [Prompt("Voce está de acordo com o combinado na negociação? {||}")]
+       // public bool Fala2 { get; set; }
 
 
         public Service ServiceRequired { get; set; }
@@ -21,13 +28,31 @@ namespace formflow.FormFlow
         {
             Consultancy, Support, ProjectDelivery, Unknown
         }
-
-        public static IForm<Enquiry> BuildEnquiryForm()                                 //Cria um formulario adicionando alguns campos, retorna um IForm
+        public static IForm<Enquiry> BuildForm()
         {
-            return new FormBuilder<Enquiry>()                                           //aqui se define a ordem da apresentação das mensagens (Bot-to-User)
-                .Field("Fala1")
-                .Field("Name")
-                .Build();
+            var builder = new FormBuilder<Enquiry>();
+            builder.Configuration.DefaultPrompt.ChoiceStyle = ChoiceStyleOptions.Auto;
+            builder.Configuration.Yes = new string[] { "sim" };
+            builder.Configuration.No = new string[] { "nao" };
+            // Builds an IForm<T> based on BasicForm
+            return builder.Build();
         }
+
+        public static IFormDialog<Enquiry> BuildFormDialog(FormOptions options = FormOptions.PromptInStart)
+        {
+            // Generated a new FormDialog<T> based on IForm<BasicForm>
+            return FormDialog.FromForm(BuildForm, options);
+        }
+
+
+      //  public static IForm<Enquiry> BuildEnquiryForm()                                 //Cria um formulario adicionando alguns campos, retorna um IForm
+      //  {
+      //      return new FormBuilder<Enquiry>()                                           //aqui se define a ordem da apresentação das mensagens (Bot-to-User)
+      //          .Field("Fala1")
+      //          .Field("Name")
+      //          .Build();
+      //  }
+
+
     }
 }
